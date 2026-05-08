0f7c053f21169dc6e317c13c88a1c022117db013
Mark McDermott <mark@markmcdermott.io>
2026-05-07 21:52:10 -0500

    fix(page): add useScrollToTop hook to each page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,10 @@
 import { Outlet } from 'react-router'
 import { Footer } from './Footer'
+import { useScrollToTop } from '../hooks/useScrollToTop'
 
 export function Layout() {
+  useScrollToTop()
+  
   return (
     <div className="flex min-h-screen flex-col text-ink">
       <Outlet />
