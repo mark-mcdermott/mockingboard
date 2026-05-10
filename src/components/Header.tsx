d5bbcd41ece1623bfade862aadc3b37b3bdbf8e7
Mark McDermott <mark@markmcdermott.io>
2026-05-10 15:19:14 -0500

    fix(misc): reduce animation for a11y & remove dead code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,6 +17,7 @@ export function Header({ children }: HeaderProps) {
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth=".2"
+          aria-hidden="true"
         >
           <path d="M8.97.5l-2.5,6,4,13.4,4-13.4L11.97.5"/>
           <path d="M15.47.5c.6,0,1.2.3,1.6.8l3,4c.5.7.5,1.7,0,2.4l-8,11c-1.7,2-1.81,1.7-2.79.48-.6-.75-.66-.8-.82-1.05L.88,7.7c-.5-.7-.5-1.7,0-2.4L3.88,1.3c.4-.5,1-.8,1.6-.8h10Z" strokeWidth="1"/>
